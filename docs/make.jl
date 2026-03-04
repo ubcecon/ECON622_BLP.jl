f9f73c2c68817f4d2473b6cc8c48bae073856324
Paul Schrimpf <paul.schrimpf@ubc.ca>
2026-03-04 14:12:38 -0800

    working on CI

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,17 @@
 using Documenter, ECON622_BLP
 makedocs(;
-         modules=[ECON622_BLP],
-         format=Documenter.HTML(),
-         pages=[
-           "Home" => "index.md",
-           "Function Reference" => "functions.md"
-         ],
-         repo=Remotes.GitHub("UBCECON","ECON622_BLP.jl"),
-         sitename="ECON622_BLP.jl",
-         #doctest=false,
-         authors="Paul Schrimpf <schrimpf@mail.ubc.ca>"
-         #assets=String[],
-         #plugins=[bib]
+    modules=[ECON622_BLP],
+    format=Documenter.HTML(),
+    authors="Paul Schrimpf <paul.schrimpf@ubc.ca> and contributors",
+    pages=[
+        "Home" => "index.md",
+        "Function Reference" => "functions.md"
+    ],
+    repo=Remotes.GitHub("UBCECON","ECON622_BLP.jl"),
+    sitename="ECON622_BLP.jl",
+    #doctest=false,
+    #assets=String[],
+    #plugins=[bib]
 )
-deploydocs(repo="github.com/UBCECON/ECON622_BLP.jl.git", branch="gh-pages")
+
+deploydocs(;repo="github.com/UBCECON/ECON622_BLP.jl.git", branch="gh-pages")
